0b4960a8b0ce85ee67f46daab47b726cb95803b8
Pedro <pedrodias504@gmail.com>
2023-05-03 00:48:56 -0300

    React - Implementing the page header component

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,10 +1,11 @@
+import Header from "./components/header/Header";
 import './App.css'
 
 function App() {
 
     return (
         <>
-            <h1>Hello LibMVC</h1>
+            <Header/>
         </>
     )
 }
---
+++ React/src/components/header/Header.jsx
@@ -0,0 +1,18 @@
+import { React } from "react";
+import Menu from "../menu/Menu";
+import menuIcon from "./assets/menu_icon.svg";
+import "./Header.css";
+
+export default function Header() {
+    return (
+        <>
+            <header className="header-container">
+                <button className="header-menu-button">
+                    <img src={menuIcon} alt="menu"/>
+                </button>
+                <h1>LibMVC</h1>
+            </header>
+            <Menu activePage={"Acervo"}/>
+        </>
+    )
+}

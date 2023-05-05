5397a00cce513fe55c7799a55516e08c1232727b
Pedro <pedrodias504@gmail.com>
2023-05-04 21:11:03 -0300

    React - Minor CSS fixes

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Header from "./components/header/Header";
 import Collection from "./pages/Collection";
 import Loans from "./pages/Loans";
-import './App.css'
 
 function App() {
 

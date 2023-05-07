ef01de74830387fac2da02da5b9bf88b6b3e79f6
Pedro <pedrodias504@gmail.com>
2023-05-07 19:03:55 -0300

    React - Implementing a TextArea component

---
+++ React/src/components/textarea/TextArea.jsx
@@ -0,0 +1,48 @@
+import { React, useState } from "react";
+import clearIcon from "./assets/cancel_icon.svg";
+import "./TextArea.css";
+
+
+export default function TextArea({
+    label = "",
+    supportingText = "",
+    minLength,
+    maxLength,
+    required,
+    defaultValue = "",
+    error,
+    errorMessage,
+    value,
+    onChange
+}) {
+    let [isActive, setIsActive] = useState(false);
+
+    return (
+        <div
+            className={`textarea-container${(isActive) ? " textarea-container-active" : ""}${(error) ? " textarea-container-error" : ""}`}
+        >
+            <label>{label}</label>
+            <div className={`textarea-control${(error) ? " textarea-control-error" : ""}`}>
+                <textarea
+                    value={value}
+                    minLength={minLength}
+                    maxLength={maxLength}
+                    required={required}
+                    onChange={(e) => onChange(e.target.value)}
+                    onFocus={() => setIsActive(true)}
+                    onBlur={() => setIsActive(false)}
+                />
+                <button
+                    onClick={(e) => {
+                        onChange(defaultValue);
+                        e.preventDefault();
+                    }}
+                >
+                    <img src={clearIcon} alt="limpar"/>
+                </button>
+            </div>
+            {(!error && supportingText) && <span className="textarea-support">{supportingText}</span>}
+            {(error && errorMessage) && <span className="textarea-error-message">{errorMessage}</span>}
+        </div>
+    )
+}

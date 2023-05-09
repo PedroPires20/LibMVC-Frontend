50dc4d5b09b4f0d1c2ec842767cfc1221d22ec8d
Pedro <pedrodias504@gmail.com>
2023-05-09 00:13:45 -0300

    React - Implementing the collection page (static)

--- React/src/pages/Collection.jsx
+++ React/src/pages/Collection.jsx
@@ -1,10 +1,58 @@
-import { React } from "react";
+import { React} from "react";
+import SearchBox from "../components/search_box/SearchBox";
+import Select from "../components/select/Select";
+import Button from "../components/button/Button";
+import { TableCard, TableHeader, TableRow, TableCell } from "../components/table_card/TableCard";
+import "./Collection.css";
 
 
 export default function Collection() {
     return (
-        <div>
-            <p>Collection</p>
+        <div className="collection-page">
+            <h2>Acervo</h2>
+            <div className="collection-input-card">
+                <div className="collection-input-titles">
+                    <h3>Pesquisa no acervo:</h3>
+                    <h3>Refine sua busca:</h3>
+                </div>
+                <div className="collection-inputs">
+                    <div className="collection-search-box-container">
+                        <SearchBox/>
+                    </div>
+                    <div className="collection-select-container">
+                        <Select label="Autor" placeholder="Todos"/>
+                    </div>
+                    <div className="collection-select-container">
+                        <Select label="Categoria" placeholder="Todos"/>
+                    </div>
+                    <div className="collection-select-container">
+                        <Select label="Editora" placeholder="Todos"/>
+                    </div>
+                    <div className="collection-select-container">
+                        <Select label="Formato" placeholder="Todos"/>
+                    </div>
+                    <div className="collection-button-container">
+                        <Button variant="secondary">Pesquisar</Button>
+                    </div>
+                </div>
+            </div>
+            <div className="collection-table-container">
+                <TableCard>
+                    <TableHeader>
+                        <TableRow>
+                            <TableCell>ISBN</TableCell>
+                            <TableCell>Autor(es)</TableCell>
+                            <TableCell>Categorias</TableCell>
+                            <TableCell>Editora</TableCell>
+                            <TableCell>Edição</TableCell>
+                            <TableCell>Formato</TableCell>
+                            <TableCell>Páginas</TableCell>
+                            <TableCell>Cópias</TableCell>
+                            <TableCell>Localização</TableCell>
+                        </TableRow>
+                    </TableHeader>
+                </TableCard>
+            </div>
         </div>
     )
 }

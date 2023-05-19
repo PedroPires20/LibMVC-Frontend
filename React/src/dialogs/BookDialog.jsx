5294d40f2c2ae429644827bb74b89f710322c6aa
Pedro <pedrodias504@gmail.com>
2023-05-18 23:16:23 -0300

    React - Adds state/validation to the book dialog

--- React/src/dialogs/BookDialog.jsx
+++ React/src/dialogs/BookDialog.jsx
@@ -1,4 +1,4 @@
-import { React } from "react";
+import { React, useState } from "react";
 import DialogBox from "../components/dialog_box/DialogBox";
 import Input from "../components/input/Input";
 import TextArea from "../components/textarea/TextArea";
@@ -8,30 +8,142 @@ import "./BookDialog.css";
 
 
 export default function BookDialog({ onClose }) {
+    const [bookData, setBookData] = useState({
+        isbn: "",
+        title: "",
+        author: "",
+        categories: "",
+        publisher: "",
+        edition: "",
+        format: "",
+        date: "",
+        pages: "",
+        copies: "",
+        description: "",
+        location: ""
+    });
+
+    function handleInputChange(name, value) {
+        setBookData({ ...bookData, [name]: value });
+    }
+
+    function handleFormSubmit(event) {
+        event.preventDefault();
+
+        if(event.target.checkValidity()) {
+            console.log("valid");
+        }else {
+            console.log("invalid!");
+        }
+    }
+
     return (
         <DialogBox>
             <div className="book-dialog-container">
                 <h3>Adicionar novo livro</h3>
                 <p>Preencha as informações abaixo para cadastrar um novo livro</p>
-                <form name="book-dialog-form" onSubmit={(e) => e.preventDefault()}>
-                    <Input type="text" label="ISBN" supportingText="Entre o ISBN do livro"/>
-                    <Input type="text" label="Título" supportingText="Entre o título do livro"/>
-                    <Input type="text" label="Autor(es)" supportingText="Entre o(s) autor(es) do livro"/>
+                <form name="book-dialog-form" noValidate onSubmit={handleFormSubmit}>
+                    <Input
+                        name="isbn"
+                        type="text"
+                        label="ISBN"
+                        supportingText="Entre o ISBN do livro"
+                        value={bookData.isbn}
+                        onChange={handleInputChange}
+                    />
+                    <Input
+                        name="title"
+                        type="text"
+                        label="Título"
+                        supportingText="Entre o título do livro"
+                        value={bookData.title}
+                        onChange={handleInputChange}
+                        required
+                    />
+                    <Input
+                        name="author"
+                        type="text"
+                        label="Autor(es)"
+                        supportingText="Entre o(s) autor(es) do livro"
+                        value={bookData.author}
+                        onChange={handleInputChange}
+                        required
+                    />
                     <Select
+                        name="categories"
                         label="Categorias"
                         options={["Nova categoria"]}
                         placeholder="Entre a(s) categoria(s) do livro"
+                        value={bookData.categories}
+                        onChange={handleInputChange}
                         multiple
                         formVariant
                     />
-                    <Input type="text" label="Editora" supportingText="Entre a editora do livro"/>
-                    <Input type="text" label="Edição" supportingText="Entre a edição do livro"/>
-                    <Input type="text" label="Formato" supportingText="Entre o formato do livro"/>
-                    <DatePicker label="Data de publicação" supportingText="Selecione a data de publicação do livro"/>
-                    <Input type="number" label="Páginas" supportingText="Entre o número de páginas do livro"/>
-                    <Input type="number" label="Cópias" supportingText="Entre o número de cópias disponíveis do livro"/>
-                    <TextArea label="Descrição" supportingText="Entre uma breve descrição para o livro"/>
-                    <Input type="text" label="Localização" supportingText="Entre a localização do livro no acervo"/>
+                    <Input
+                        name="publisher"
+                        type="text"
+                        label="Editora"
+                        supportingText="Entre a editora do livro"
+                        value={bookData.publisher}
+                        onChange={handleInputChange}
+                    />
+                    <Input
+                        name="edition"
+                        type="text"
+                        label="Edição"
+                        supportingText="Entre a edição do livro"
+                        value={bookData.edition}
+                        onChange={handleInputChange}
+                    />
+                    <Input
+                        name="format"
+                        type="text"
+                        label="Formato"
+                        supportingText="Entre o formato do livro"
+                        value={bookData.format}
+                        onChange={handleInputChange}
+                    />
+                    <DatePicker
+                        name="date"
+                        label="Data de publicação"
+                        supportingText="Selecione a data de publicação do livro"
+                        value={bookData.date}
+                        onChange={handleInputChange}
+                    />
+                    <Input
+                        name="pages"
+                        type="number"
+                        label="Páginas"
+                        supportingText="Entre o número de páginas do livro"
+                        minValue={1}
+                        value={bookData.pages}
+                        onChange={handleInputChange}
+                    />
+                    <Input
+                        name="copies"
+                        type="number"
+                        label="Cópias"
+                        supportingText="Entre o número de cópias disponíveis do livro"
+                        minValue={1}
+                        value={bookData.copies}
+                        onChange={handleInputChange}
+                        required
+                    />
+                    <TextArea
+                        name="description"
+                        label="Descrição"
+                        supportingText="Entre uma breve descrição para o livro"
+                        value={bookData.description}
+                        onChange={handleInputChange}
+                    />
+                    <Input
+                        name="location"
+                        type="text"
+                        label="Localização"
+                        supportingText="Entre a localização do livro no acervo"
+                        value={bookData.location}
+                        onChange={handleInputChange}
+                    />
                     <div className="book-dialog-buttons">
                         <button
                             className="click-ripple-effect-light"

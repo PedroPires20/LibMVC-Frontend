cc5e891c21f4e4a6e68153ab3e76081b56f5b148
Pedro <pedrodias504@gmail.com>
2023-05-25 22:51:17 -0300

    React - Adds a simple load status component

---
+++ React/src/components/load_status/LoadStatus.jsx
@@ -0,0 +1,21 @@
+import { React } from "react";
+import ProgressBar from "../progress_bar/ProgressBar";
+import errorIcon from "./assets/error_icon.svg";
+import "./LoadStatus.css";
+
+
+export default function LoadStatus({ error, loadingMessage, errorMessage }) {
+    return (
+        <div className="load-status-container">
+            {(error) ? (
+                <img src={errorIcon} alt="Erro"/>
+            ) : (
+                <ProgressBar/>
+            )
+            }
+            <p>
+                {(error) ? errorMessage : loadingMessage}
+            </p>
+        </div>
+    )
+}

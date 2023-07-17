340f41453e965dd255d040bf024b93d278cdb99e
Pedro Dias Pires <pedrodias504@gmail.com>
2023-07-17 16:06:38 -0300

    Angular - Adds form logic to the select component

--- Angular/src/app/components/select/select.component.ts
+++ Angular/src/app/components/select/select.component.ts
@@ -1,18 +1,26 @@
-import { Component, Input, OnChanges, ElementRef, HostListener, SimpleChanges } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnChanges, ElementRef, HostListener, SimpleChanges, forwardRef } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'app-select',
   templateUrl: './select.component.html',
-  styleUrls: ['./select.component.css']
+  styleUrls: ['./select.component.css'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => SelectComponent),
+      multi: true
+    }    
+  ]
 })
-export class SelectComponent implements OnChanges {
+export class SelectComponent implements OnChanges, ControlValueAccessor {
   constructor(selectElement: ElementRef) {
     this.selectElementRef = selectElement;
   }
 
   ngOnChanges(changes: SimpleChanges) {
-    if(changes["options"] || changes["value"]) {
-      this.values = (this.optionValues.length > 0) ? this.optionValues : this.options;
+    if(changes["options"] || changes["optionValues"]) {
+      this.optionValuesMap = (this.optionValues.length > 0) ? this.optionValues : this.options;
     }
   }
 
@@ -21,6 +29,63 @@ export class SelectComponent implements OnChanges {
     if(!this.selectElementRef.nativeElement.contains(event.target)) {
       this.active = false;
     }
+  }
+
+  writeValue(newValue: string | string[]) {
+    this.value = newValue;
+  }
+  
+  registerOnChange(fn: any) {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    
+  }
+  
+  setDisabledState(isDisabled: boolean) {
+    this.disabled = isDisabled;
+  }
+
+  handleOptionClick(event: MouseEvent, optionIndex: number) {
+    if(this.disabled) {
+      this.active = false;
+      event.stopPropagation();
+      return;
+    }
+    if(this.multiple) {
+      if(this.selectedIndexes.includes(optionIndex)) {
+        this.selectedIndexes = this.selectedIndexes.filter((selectedIndex) => selectedIndex !== optionIndex);
+      }else {
+        this.selectedIndexes.push(optionIndex);
+      }
+      this.value = this.selectedIndexes.map((selectedIndex) => this.optionValuesMap[selectedIndex]);
+      this.onChange(this.value);
+    }else {
+      event.stopPropagation();
+      this.selectedIndexes = [optionIndex];
+      this.active = false;
+      this.value = this.optionValuesMap[optionIndex];
+      this.onChange(this.value);
+    }
+  }
+
+  handleClearButtonClick(event: MouseEvent) {
+    event.preventDefault();
+    event.stopPropagation();
+    this.active = false;
+    this.selectedIndexes = [];
+    this.value = this.multiple ? [] : "";
+    this.onChange(this.value);
+  }
+
+  generateInputValue() {
+    if(this.selectedIndexes.length > 0) {
+      return this.selectedIndexes.map(
+        (optionIndex) => this.optionValuesMap[optionIndex]
+      ).join("; ");
+    }
+    return "";
   }
 
   @Input() name = "";
@@ -33,9 +98,11 @@ export class SelectComponent implements OnChanges {
   @Input() errorMessage = "Por favor, selecione uma opção";
   @Input() disabled = false;
   @Input() required = false;
-
-  active = true;
-  values = (this.optionValues.length > 0) ? this.optionValues : this.options;
+  
+  active = false;
+  value: string | string[] = "";
+  onChange: any = () => {};
+  optionValuesMap = (this.optionValues.length > 0) ? this.optionValues : this.options;
   selectedIndexes: number[] = [];
   private selectElementRef: ElementRef;
 }

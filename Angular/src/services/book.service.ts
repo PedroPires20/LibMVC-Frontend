f1f593610a21697e3f10519057df17b471e0ce37
Pedro Dias Pires <pedrodias504@gmail.com>
2023-07-23 01:23:57 -0300

    Angular - Implements caching on the books load

--- Angular/src/services/book.service.ts
+++ Angular/src/services/book.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import NetworkClient from '@common/utils/network_client.js';
 import Book from '@common/models/book.js';
+import { removeEmptyFilters, objectEquals } from '@common/utils/utils.js';
 
 
 @Injectable({
@@ -12,20 +13,27 @@ export class BookService {
     this._api = new NetworkClient(environment.API_BASE_URL);
     this._selectedBooks = [];
     this._errorMessage = "";
-
   }
 
   async fetchBooks(query = "", filters: any = {}) {
-    this._status = "loading";
-    try {
-      let booksData = await this._api.searchBooks(query, filters, { title: 1 });
-      this._selectedBooks = booksData.map((bookData: any, index: number) => new Book(bookData, index));
-      this._status = "ok";
-    }catch(exception: any) {
-      console.error(exception);
-      this._status = "error";
-      this._errorMessage = exception?.message;
+    if(this._shouldRefetchBooks(query, filters)) {
+      this._status = "loading";
+      try {
+        let booksData = await this._api.searchBooks(query, removeEmptyFilters(filters), { title: 1 });
+        this._selectedBooks = booksData.map((bookData: any, index: number) => new Book(bookData, index));
+        this._status = "loaded";
+      }catch(exception: any) {
+        console.error(exception);
+        this._status = "error";
+        this._errorMessage = exception?.message;
+      }
+      this._previousQuery = query;
+      this._previousFilters = filters;
     }
+  }
+
+  private _shouldRefetchBooks(query: string, filters: any) {
+    return (this._previousQuery !== query) || (!objectEquals(this._previousFilters, filters));
   }
 
   get status() {
@@ -42,6 +50,8 @@ export class BookService {
 
   private _api: NetworkClient;
   private _selectedBooks: Book[];
-  private _status?: "loading" | "error" | "ok";
+  private _status?: "loading" | "error" | "loaded";
   private _errorMessage: string;
+  private _previousQuery?: string;
+  private _previousFilters: any;
 }

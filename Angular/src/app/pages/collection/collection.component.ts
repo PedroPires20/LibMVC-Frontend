8ddb5a307e5d935fbdd58e1f1e8253bb3b4be838
Pedro Dias Pires <pedrodias504@gmail.com>
2023-07-23 01:25:31 -0300

    Angular - Fetching books via API on the collection

--- Angular/src/app/pages/collection/collection.component.ts
+++ Angular/src/app/pages/collection/collection.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BookService } from 'src/services/book.service';
 
 @Component({
   selector: 'app-collection',
   templateUrl: './collection.component.html',
   styleUrls: ['./collection.component.css']
 })
-export class CollectionComponent {
+export class CollectionComponent implements OnInit {
+  constructor(private _bookService: BookService) {
+
+  }
+
+  ngOnInit(): void {
+    this._bookService.fetchBooks();
+  }
+
+  get booksLoading() {
+    return this._bookService.status === "loading" || !this._bookService.status;
+  }
+
+  get errorLoading() {
+    return this._bookService.status === "error";
+  }
+
+  get statusMessage() {
+    switch(this._bookService.status) {
+      case "loading": return "Carregando dados do acervo...";
+      case "error": return "Ocorreu um erro ao recuperar os dados do acervo. Por favor, tente novamente.";
+      default: return "Nenhum livro foi encontrado";
+    }
+  }
+
+  get books() {
+    return this._bookService.selectedBooks;
+  }
 
 }

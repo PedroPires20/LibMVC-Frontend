80a580fef34f4925b7a17131d1dc88638a6ea9aa
Pedro <pedrodias504@gmail.com>
2023-07-25 22:24:49 -0300

    Angular - Implements book deletion on Collection

--- Angular/src/app/pages/collection/collection.component.ts
+++ Angular/src/app/pages/collection/collection.component.ts
@@ -2,6 +2,16 @@ import { Component, OnInit } from '@angular/core';
 import { BookService } from 'src/services/book.service';
 import { ContextMenuPosition } from 'src/app/components/context-menu/context-menu.component';
 
+interface BookDeleteState {
+  variant: "delete" | "error" | "load",
+  heading: string,
+  message: string,
+  buttonLabels?: string[],
+  summary?: string,
+  details?: string
+}
+
+
 @Component({
   selector: 'app-collection',
   templateUrl: './collection.component.html',
@@ -26,13 +36,48 @@ export class CollectionComponent implements OnInit {
   }
 
   handleContextMenuClose(action?: number) {
+    this.showContextMenu = false;
     if(action === 0) {
 
     }else if(action === 1) {
-
+      this.deleteDialogState = {
+        variant: "delete",
+        heading: "Apagar Livro?",
+        message: `Deseja remover o livro "${this.books[this.clickTargetIndex || 0].title}" do acervo?\nUma vez excluído, as informações desse livro não poderão ser recuperadas!`,
+        buttonLabels: ["Sim", "Não"]
+      };
     }
-    this.showContextMenu = false;
-    this.clickTargetIndex = undefined;
+  }
+
+  handleDeleteDialogClose(action: number) {
+    if(this.deleteDialogState?.variant === "delete" && action === 0
+      && this.clickTargetIndex !== null) {
+        this._handleBookDelete()
+    }else {
+      this.deleteDialogState = null;
+    }
+  }
+
+  async _handleBookDelete() {
+    if(this.clickTargetIndex !== null) {
+      this.deleteDialogState = {
+        variant: "load",
+        heading: "Salvando",
+        message: "As alterações estão sendo processadas pelo sistema"
+      }
+      let result = await this._bookService.deleteBook(this.clickTargetIndex);
+      if(result) {
+        this.deleteDialogState = {
+          variant: "error",
+          heading: "Erro",
+          message: "Ocorreu um erro ao salvar as alterações no sistema.",
+          summary: "Detalhes do erro",
+          details: result
+        };
+        return;
+      }
+    }
+    this.deleteDialogState = null;
   }
 
   get booksLoading() {
@@ -57,5 +102,6 @@ export class CollectionComponent implements OnInit {
 
   showContextMenu = false;
   contextMenuPosition: ContextMenuPosition = { x: 0, y: 0 };
-  clickTargetIndex?: number;
+  clickTargetIndex: number | null = null;
+  deleteDialogState: BookDeleteState | null = null;
 }

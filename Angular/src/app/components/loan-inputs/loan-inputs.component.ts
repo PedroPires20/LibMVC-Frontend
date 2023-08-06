3050225c5042170f607d8853bf722808f650cb7c
Pedro Dias Pires <pedrodias504@gmail.com>
2023-08-05 23:48:30 -0300

    Angular - Implements the filters on the loans page

--- Angular/src/app/components/loan-inputs/loan-inputs.component.ts
+++ Angular/src/app/components/loan-inputs/loan-inputs.component.ts
@@ -1,11 +1,71 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LoansService } from 'src/services/loans.service';
+import { LoanField, LoanFieldsService } from 'src/services/loan-fields.service';
+
+interface Filters {
+  reader?: string,
+  bookTitle?: string,
+  startDate?: string,
+  endDate?: string,
+  late?: boolean,
+  renew?: boolean
+}
+
+const FIELD_LOADING_MESSAGE = "Carregando...";
+const FIELD_LOADING_ERROR = "Ocorreu um erro ao carregar as opções do filtro";
+
 
 @Component({
   selector: 'app-loan-inputs',
   templateUrl: './loan-inputs.component.html',
   styleUrls: ['./loan-inputs.component.css']
 })
-export class LoanInputsComponent {
+export class LoanInputsComponent implements OnInit {
+  constructor(private _loansService: LoansService, private _loanFieldsService: LoanFieldsService) {
 
-  @Input() disabled = false;
+  }
+
+  ngOnInit(): void {
+      this._loanFieldsService.fetchFields();
+  }
+
+  handleFilter() {
+    this._loansService.fetchLoans(this.filters);
+  }
+
+  handleReset() {
+    this.filters = {};
+    this._loansService.fetchLoans();
+  }
+
+  get readerSelectorDisabled() {
+    return this._loanFieldsService.reader.loadStatus !== "loaded";
+  }
+
+  get readerOptions() {
+    return this._handleFieldLoad(this._loanFieldsService.reader);
+  }
+
+  get bookTitleSelectorDisabled() {
+    return this._loanFieldsService.bookTitle.loadStatus !== "loaded";
+  }
+
+  get bookTitleOptions() {
+    return this._handleFieldLoad(this._loanFieldsService.bookTitle);
+  }
+
+  get disabled() {
+    return this._loansService.status === "loading" || this._loansService.status === "error";
+  }
+
+  private _handleFieldLoad(filed: LoanField) {
+    if(filed.loadStatus === "loading") {
+      return [FIELD_LOADING_MESSAGE];
+    }else if(filed.loadStatus === "error") {
+      return [FIELD_LOADING_ERROR];
+    }
+    return filed.values;
+  }
+
+  filters: Filters = {};
 }

8bbe2a034876ddf1e93b169d68d4d5a5f3123e12
Pedro Dias Pires <pedrodias504@gmail.com>
2023-08-14 16:50:21 -0300

    Svelte - Implementing a progress bar component

---
+++ Svelte/src/components/ProgressBar.svelte
@@ -0,0 +1,44 @@
+<script>
+    export let width = "25rem";
+    export let value;
+
+    $: indefinite = (typeof value !== "number") || value < 0 || value > 100;
+</script>
+
+<style>
+    div {
+        width: 25rem;
+        height: 0.3rem;
+        background-color: #efe0cf;
+        position: relative;
+        overflow: hidden;
+    }
+
+    div::before {
+        content: "";
+        display: block;
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: var(--filled-width);
+        height: 100%;
+        background-color: #7e5700;
+
+    }
+
+    div.indefinite::before {
+        width: 100%;
+        animation: move-indefinite-progress-bar 1.5s ease-in-out infinite;
+    }
+
+    @keyframes move-indefinite-progress-bar {
+        0% {
+        transform: translateX(-100%);
+        }
+        100% {
+        transform: translateX(100%);
+        }
+    }
+</style>
+
+<div class:indefinite style:width style:--filled-width={value + "%"}/>

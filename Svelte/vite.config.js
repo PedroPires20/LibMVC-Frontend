c4f83189312082edc5a89a93d1468ff3cc9102fe
Pedro Dias Pires <pedrodias504@gmail.com>
2023-08-19 17:03:51 -0300

    Svelte - Configures the app to use the common code

--- Svelte/vite.config.js
+++ Svelte/vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  resolve: {
+    alias: {
+      "@common": path.resolve(__dirname, "../common")
+    }
+  },
+  define: {
+    API_BASE_URL: JSON.stringify("http://localhost:3000")
+  }
 })

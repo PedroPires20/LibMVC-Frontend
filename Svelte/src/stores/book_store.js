0121e69e4b6e8c549042dfe24274cdf81a0b1527
Pedro <pedrodias504@gmail.com>
2023-08-19 20:21:01 -0300

    Svelte - Initial books store implementation

---
+++ Svelte/src/stores/book_store.js
@@ -0,0 +1,31 @@
+import { writable } from "svelte/store";
+import NetworkClient from "@common/utils/network_client";
+import Book from "@common/models/book";
+import { objectEquals } from "@common/utils/utils";
+
+
+export function createBooks() {
+    const { set, update, subscribe } = writable([]);
+    let networkClient = new NetworkClient(API_BASE_URL);
+    let previousQuery;
+    let previousFilters;
+
+    async function queryBooks(queryText = "", filters = {}) {
+        if(queryText !== previousQuery && !objectEquals(filters, previousFilters)) {
+            try {
+                let booksData = await networkClient.searchBooks(queryText, filters, { title: 1 });
+                set(booksData.map((bookData, index) => new Book(bookData, index)));
+                previousQuery = queryText;
+                previousFilters = filters;
+            }catch(exception) {
+                console.error("Error loading books: " + exception);
+                throw exception;
+            }
+        }
+    }
+
+    return {
+        subscribe,
+        queryBooks
+    };
+}

27c899b3e0449e2f4f3630ce2bde8a7017c0a2f4
Pedro <pedrodias504@gmail.com>
2023-08-23 21:39:02 -0300

    Svelte - Adds prop to control the book save status

--- Svelte/src/dialogs/BookDialog.svelte
+++ Svelte/src/dialogs/BookDialog.svelte
@@ -1,6 +1,7 @@
 <script>
     import { createEventDispatcher } from "svelte";
     import DialogBox from "../components/DialogBox.svelte";
+    import StateDialog from "./StateDialog.svelte";
     import Input from "../components/form_components/Input.svelte";
     import Select from "../components/form_components/Select.svelte";
     import DatePicker from "../components/form_components/DatePicker.svelte";
@@ -25,6 +26,7 @@
     const dispatch = createEventDispatcher();
 
     export let updateTarget = undefined;
+    export let saveStatus = { saving: false, error: false };
 
     let { categories } = createBookFields();
     let addCategory = false;
@@ -112,132 +114,143 @@
 </style>
 
 <DialogBox>
-    <div class="container">
-        <h3>{(isUpdateDialog) ? "Editar livro" : "Adicionar novo livro"}</h3>
-        <p>
-            {(isUpdateDialog) ? "Edite, abaixo, as informações desejadas e confirme suas alterações"
-                : "Preencha as informações abaixo para cadastrar um novo livro"}
-        </p>
-        <form
-            name="book-dialog-form"
-            novalidate
-            on:submit|preventDefault={(event) => handleFormSubmit(event)}
-        >
-            <Input
-                name="isbn"
-                type="text"
-                label="ISBN"
-                supportingText="Entre o ISBN do livro"
-                required
-                bind:value={bookData.isbn}
-            />
-            <Input
-                name="title"
-                type="text"
-                label="Título"
-                supportingText="Entre o título do livro"
-                required
-                bind:value={bookData.title}
-            />
-            <Input
-                name="author"
-                type="text"
-                label="Autor(es)"
-                supportingText="Entre o(s) autor(es) do livro"
-                required
-                bind:value={bookData.author}
-            />
-            {#if addCategory}
+    {#if !saveStatus.saving && !saveStatus.error}
+        <div class="container">
+            <h3>{(isUpdateDialog) ? "Editar livro" : "Adicionar novo livro"}</h3>
+            <p>
+                {(isUpdateDialog) ? "Edite, abaixo, as informações desejadas e confirme suas alterações"
+                    : "Preencha as informações abaixo para cadastrar um novo livro"}
+            </p>
+            <form
+                name="book-dialog-form"
+                novalidate
+                on:submit|preventDefault={(event) => handleFormSubmit(event)}
+            >
                 <Input
-                    name="new-category"
+                    name="isbn"
                     type="text"
-                    label="Nova categoria"
-                    supportingText="Entre o rótulo da nova categoria e pressione enter"
-                    autofocus
-                    bind:value={newCategory}
-                    on:keydown={handleCategoryAdd} 
+                    label="ISBN"
+                    supportingText="Entre o ISBN do livro"
+                    required
+                    bind:value={bookData.isbn}
                 />
-            {:else}
-                <Select
-                    name="categories"
-                    label="Categorias"
-                    placeholder="Entre a(s) categoria(s) do livro"
-                    options={categoryOptions}
+                <Input
+                    name="title"
+                    type="text"
+                    label="Título"
+                    supportingText="Entre o título do livro"
+                    required
+                    bind:value={bookData.title}
+                />
+                <Input
+                    name="author"
+                    type="text"
+                    label="Autor(es)"
+                    supportingText="Entre o(s) autor(es) do livro"
+                    required
+                    bind:value={bookData.author}
+                />
+                {#if addCategory}
+                    <Input
+                        name="new-category"
+                        type="text"
+                        label="Nova categoria"
+                        supportingText="Entre o rótulo da nova categoria e pressione enter"
+                        autofocus
+                        bind:value={newCategory}
+                        on:keydown={handleCategoryAdd} 
+                    />
+                {:else}
+                    <Select
+                        name="categories"
+                        label="Categorias"
+                        placeholder="Entre a(s) categoria(s) do livro"
+                        options={categoryOptions}
+                        formVariant
+                        multiple
+                        bind:value={bookData.categories}
+                    />
+                {/if}
+                <Input
+                    name="publisher"
+                    type="text"
+                    label="Editora"
+                    supportingText="Entre a editora do livro"
+                    bind:value={bookData.publisher}
+                />
+                <Input
+                    name="edition"
+                    type="text"
+                    label="Edição"
+                    supportingText="Entre a edição do livro"
+                    bind:value={bookData.edition}
+                />
+                <Input
+                    name="format"
+                    type="text"
+                    label="Formato"
+                    supportingText="Entre o formato do livro"
+                    bind:value={bookData.format}
+                />
+                <DatePicker
+                    name="date"
+                    label="Data de publicação"
+                    supportingText="Selecione a data de publicação do livro"
                     formVariant
-                    multiple
-                    bind:value={bookData.categories}
+                    bind:value={bookData.date}
                 />
-            {/if}
-            <Input
-                name="publisher"
-                type="text"
-                label="Editora"
-                supportingText="Entre a editora do livro"
-                bind:value={bookData.publisher}
-            />
-            <Input
-                name="edition"
-                type="text"
-                label="Edição"
-                supportingText="Entre a edição do livro"
-                bind:value={bookData.edition}
-            />
-            <Input
-                name="format"
-                type="text"
-                label="Formato"
-                supportingText="Entre o formato do livro"
-                bind:value={bookData.format}
-            />
-            <DatePicker
-                name="date"
-                label="Data de publicação"
-                supportingText="Selecione a data de publicação do livro"
-                formVariant
-                bind:value={bookData.date}
-            />
-            <Input
-                name="pages"
-                type="number"
-                label="Páginas"
-                supportingText="Entre o número de páginas do livro"
-                minValue="1"
-                required
-                bind:value={bookData.pages}
-            />
-            <Input
-                name="copies"
-                type="number"
-                label="Cópias"
-                supportingText="Entre o número de cópias disponíveis do livro"
-                minValue="1"
-                required
-                bind:value={bookData.copies}
-            />
-            <TextArea
-                name="description"
-                label="Descrição"
-                supportingText="Entre uma breve descrição para o livro"
-                bind:value={bookData.description}
-            />
-            <Input
-                name="location"
-                type="text"
-                label="Localização"
-                supportingText="Entre a localização do livro no acervo"
-                bind:value={bookData.location}
-            />
-            <div class="buttons">
-                <button
-                    class="click-ripple-effect-light"
-                    on:click|preventDefault={() => dispatch("dialogclose")}
-                >
-                    Cancelar
-                </button>
-                <button class="click-ripple-effect-light">
-                    Confirmar
-                </button>
-            </div>
-        </form>
-    </div>
+                <Input
+                    name="pages"
+                    type="number"
+                    label="Páginas"
+                    supportingText="Entre o número de páginas do livro"
+                    minValue="1"
+                    required
+                    bind:value={bookData.pages}
+                />
+                <Input
+                    name="copies"
+                    type="number"
+                    label="Cópias"
+                    supportingText="Entre o número de cópias disponíveis do livro"
+                    minValue="1"
+                    required
+                    bind:value={bookData.copies}
+                />
+                <TextArea
+                    name="description"
+                    label="Descrição"
+                    supportingText="Entre uma breve descrição para o livro"
+                    bind:value={bookData.description}
+                />
+                <Input
+                    name="location"
+                    type="text"
+                    label="Localização"
+                    supportingText="Entre a localização do livro no acervo"
+                    bind:value={bookData.location}
+                />
+                <div class="buttons">
+                    <button
+                        class="click-ripple-effect-light"
+                        on:click|preventDefault={() => dispatch("dialogclose")}
+                    >
+                        Cancelar
+                    </button>
+                    <button class="click-ripple-effect-light">
+                        Confirmar
+                    </button>
+                </div>
+            </form>
+        </div>
+    {:else}
+        <StateDialog
+            variant={(saveStatus.error) ? "error" : "load"}
+            heading={(saveStatus.error) ? "Erro" : "Salvando"}
+            message={(saveStatus.error) ? "Ocorreu um erro ao salvar as alterações no sistema." : "As alterações estão sendo processadas pelo sistema"}
+            detailsSummary={(saveStatus.errorMessage && saveStatus.errorMessage !== "") && "Detalhes do erro"}
+            detailsContent={saveStatus.errorMessage}
+            on:dialogclose
+        />
+    {/if}
 </DialogBox>
